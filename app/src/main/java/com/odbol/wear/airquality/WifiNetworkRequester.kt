860b2103fcfc7cac295b09b50581960e2de85f03
Tyler Freeman <github@odbol.com>
2023-09-24 14:53:22 -0700

    downgrade SDK to 33 for Wear. upgrade kotlin. upgrade all dependencies update to java 17. comment out wifinetworkrequester due to compile error

--- app/src/main/java/com/odbol/wear/airquality/WifiNetworkRequester.kt
+++ app/src/main/java/com/odbol/wear/airquality/WifiNetworkRequester.kt
@@ -20,67 +20,67 @@ import java.util.concurrent.TimeUnit
 const val TAG = "WifiNetworkRequester"
 
 class WifiNetworkRequester(context: Context) {
-
-    val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-
-    inner class NetworkCallbackSubject : ConnectivityManager.NetworkCallback() {
-
-        val onConnected : SingleSubject<Boolean> = SingleSubject.create();
-
-        override fun onAvailable(network: Network) {
-            if (connectivityManager.bindProcessToNetwork(network)) {
-                // socket connections will now use this network
-                Log.d(TAG, "Wifi is available")
-                onConnected.onSuccess(true)
-            } else {
-                // app doesn't have android.permission.INTERNET permission
-                Log.e(TAG, "Couldn't get wifi")
-                onConnected.onSuccess(false)
-            }
-        }
-    }
-
-    fun requestWifi(): Single<Boolean> {
-        val isWifi: Boolean = connectivityManager.activeNetwork?.let { activeNetwork ->
-            connectivityManager.getNetworkCapabilities(activeNetwork).hasTransport(NetworkCapabilities.TRANSPORT_WIFI)
-        } ?: false
-
-        if (isWifi) {
-            // You already are on a high-bandwidth network, so start your network request
-            Log.d(TAG, "Already connected to wifi")
-            return Single.just(true)
-        }
-
-        return Single.using({
-            NetworkCallbackSubject()
-        }, { networkCallback ->
-
-            val request: NetworkRequest = NetworkRequest.Builder().run {
-                addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
-                addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
-                addCapability(NetworkCapabilities.NET_CAPABILITY_NOT_METERED)
-                addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
-                build()
-            }
-
-            Log.d(TAG, "Requesting Wifi")
-
-            connectivityManager.requestNetwork(request, networkCallback)
-
-            networkCallback.onConnected
-        },
-        { networkCallback ->
-            Log.d(TAG, "Disconnecting Wifi")
-            connectivityManager.bindProcessToNetwork(null)
-            connectivityManager.unregisterNetworkCallback(networkCallback)
-        })
-
-        .timeout(10, TimeUnit.SECONDS)
-        .onErrorReturn { t ->
-            Log.w(TAG, "Timed out waiting for Wifi ", t)
-            false
-        }
-
-    }
-
+//
+//    val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+//
+//    inner class NetworkCallbackSubject : ConnectivityManager.NetworkCallback() {
+//
+//        val onConnected : SingleSubject<Boolean> = SingleSubject.create();
+//
+//        override fun onAvailable(network: Network) {
+//            if (connectivityManager.bindProcessToNetwork(network)) {
+//                // socket connections will now use this network
+//                Log.d(TAG, "Wifi is available")
+//                onConnected.onSuccess(true)
+//            } else {
+//                // app doesn't have android.permission.INTERNET permission
+//                Log.e(TAG, "Couldn't get wifi")
+//                onConnected.onSuccess(false)
+//            }
+//        }
+//    }
+//
+//    fun requestWifi(): Single<Boolean> {
+//        val isWifi: Boolean = connectivityManager.activeNetwork?.let { activeNetwork ->
+//            connectivityManager.getNetworkCapabilities(activeNetwork).hasTransport(NetworkCapabilities.TRANSPORT_WIFI)
+//        } ?: false
+//
+//        if (isWifi) {
+//            // You already are on a high-bandwidth network, so start your network request
+//            Log.d(TAG, "Already connected to wifi")
+//            return Single.just(true)
+//        }
+//
+//        return Single.using({
+//            NetworkCallbackSubject()
+//        }, { networkCallback ->
+//
+//            val request: NetworkRequest = NetworkRequest.Builder().run {
+//                addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+//                addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
+//                addCapability(NetworkCapabilities.NET_CAPABILITY_NOT_METERED)
+//                addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+//                build()
+//            }
+//
+//            Log.d(TAG, "Requesting Wifi")
+//
+//            connectivityManager.requestNetwork(request, networkCallback)
+//
+//            networkCallback.onConnected
+//        },
+//        { networkCallback ->
+//            Log.d(TAG, "Disconnecting Wifi")
+//            connectivityManager.bindProcessToNetwork(null)
+//            connectivityManager.unregisterNetworkCallback(networkCallback)
+//        })
+//
+//        .timeout(10, TimeUnit.SECONDS)
+//        .onErrorReturn { t ->
+//            Log.w(TAG, "Timed out waiting for Wifi ", t)
+//            false
+//        }
+//
+//    }
+//
 }
